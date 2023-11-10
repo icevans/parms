348c1d11d045759361d7f611833b5a88474a630a
Ian Evans <icevans@icloud.com>
2023-11-10 05:46:49 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::process::exit;
+
+use clap::Parser;
+use dialoguer::Select;
+
+use ssm::get_parameters;
+
+mod ssm;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Search for parameters under this path
+    #[arg(short, long)]
+    path: String,
+
+    /// Search in this AWS region
+    #[arg(short, long, default_value_t = String::from("us-west-2"))]
+    region: String,
+}
+
+/// Fetches a parameter and displays the decrypted value
+#[tokio::main]
+async fn main() -> () {
+    let args = Args::parse();
+
+    let parameters = get_parameters(&args.path, &args.region).await;
+
+    if parameters.is_none() {
+        eprintln!("No parameters found under that path");
+        exit(1);
+    }
+
+    let parameters = parameters.unwrap();
+
+    let parameter_names: Vec<&str> = parameters
+        .iter()
+        .map(|parameter| parameter.name().unwrap_or("unnamed"))
+        .collect();
+
+    let selection = Select::new()
+        .with_prompt("What do you choose?")
+        .items(&parameter_names)
+        .interact()
+        .unwrap();
+
+    println!("{}", parameters[selection].value().unwrap());
+}

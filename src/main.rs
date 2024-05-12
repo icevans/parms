8337593fc5c37c7eae1a9787dc12913dbe21b80b
Ian Evans <icevans@icloud.com>
2024-05-11 19:49:46 -0700

    feat: switch to fuzzy finder
    My vision for this tool was as something interactive -- I can never remember the exact name of the parameter I want, nor necessarily its path hierarchy.
    
    Signed-off-by: Ian Evans <icevans@icloud.com>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use std::process::exit;
 
 use clap::Parser;
-use dialoguer::Select;
+use dialoguer::{theme::ColorfulTheme, FuzzySelect};
+
+use ssm::get_parameter_names;
 
 mod ssm;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Search for parameters under this path
-    #[arg(short, long)]
-    path: String,
-
     /// Search in this AWS region
     #[arg(short, long, default_value_t = String::from("us-west-2"))]
     region: String,
@@ -22,25 +20,21 @@ struct Args {
 async fn main() -> () {
     let args = Args::parse();
 
-    let parameters = ssm::get_parameters(&args.path, &args.region).await;
-
-    if parameters.is_none() {
-        eprintln!("No parameters found under that path");
+    let Some(parameter_names) = get_parameter_names(&args.region).await else {
+        eprintln!("No parameters exist in this region");
         exit(1);
-    }
+    };
 
-    let parameters = parameters.unwrap();
-
-    let parameter_names: Vec<&str> = parameters
-        .iter()
-        .map(|parameter| parameter.name().unwrap_or("unnamed"))
-        .collect();
-
-    let selection = Select::new()
-        .with_prompt("What do you choose?")
+    let selected_index = FuzzySelect::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select parameter (type for fuzzy search):")
+        .max_length(10)
         .items(&parameter_names)
         .interact()
         .unwrap();
 
-    println!("{}", parameters[selection].value().unwrap());
+    let value = ssm::get_parameter_value(&args.region, &parameter_names[selected_index]).await;
+    match value {
+        None => println!("oops"),
+        Some(value) => println!("{value}"),
+    }
 }
